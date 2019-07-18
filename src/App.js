4526900b64e7a72e9f918f03c4ed15ef66f3d14a
Vagacoder <qiruihu@gmail.com>
2019-07-18 13:15:14 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
 import './App.css';
 
+function EditableTable(){
+  const [dragging, setDragging] = useState(false);
+  const [listItems, setListItems] = useState([0, 1, 2]);
+
+  const colors = ['red', 'green', 'blue'];
+
+  function onDrag(){
+    setDragging(true);
+
+  }
+
+  function onDrop(){
+    setDragging(false);
+  }
+
+  function whileDragging(){
+
+  }
+
+  function getStyle(i) {
+    return {
+      background: colors[i]
+    };
+  }
+
+  function startDragging(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  return (
+    <div className="table">
+      {listItems.map(listItem => {
+       return <div key={listItem} onDragStart={startDragging} draggable="true" id={`row${listItem}`} style={getStyle(listItem)}></div>;
+      })}
+    </div>
+  );
+
+}
+
 class App extends Component {
+
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <EditableTable />
       </div>
     );
   }
