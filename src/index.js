7a3bbe1b953be88ecd6f907cec7b735b68938473
Vagacoder <qiruihu@gmail.com>
2019-07-19 00:28:08 -0700

    update dragging animation

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import SimpleSortableList from './Drag1';
 
 ReactDOM.render(
-  <SimpleSortableList />,
+  <App />,
   document.getElementById('root')
 );
