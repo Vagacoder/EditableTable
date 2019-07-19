67e3d30616413cb3879d0ab2b9766c7a0cce138d
Vagacoder <qiruihu@gmail.com>
2019-07-19 00:26:11 -0700

    update dragging animation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 
 function EditableTable() {
+
   let cpListItems = ["row 1", "row 2", "row 3", "row 4", "row 5"];
 
   const [listItems, setListItems] = useState(cpListItems);
@@ -10,11 +11,15 @@ function EditableTable() {
   const [mouseDown, setMouseDown] = useState(false);
   const [itemOnHoldStyle, setItemOnHoldStyle] = useState({
     position: 'absolute',
-    background: 'yellow',
-    opacity: 0.3,
+    "background-color": bgColor,
+    opacity: 0.75,
     display: 'none',
     pointerEvents: 'none'
   });
+
+  const [bgColor, setBgColor] = useState("yellow");
+  const [eventElementX, setOffsetX] = useState(0);
+  const [eventElementY, setOffsetY] = useState(0);
 
   function getStyle(i) {
     return {
@@ -26,10 +31,12 @@ function EditableTable() {
     if (mouseDown) {
       setItemOnHoldStyle({
         position: 'absolute',
-        background: 'yellow',
-        top: e.nativeEvent.clientY - e.nativeEvent.offsetY,
-        left: e.nativeEvent.clientX - e.nativeEvent.offsetX,
-        opacity: 0.3,
+        "background-color": bgColor,
+        //top: e.nativeEvent.clientY - e.nativeEvent.offsetY,
+        //left: e.nativeEvent.clientX - e.nativeEvent.offsetX,
+        top: e.nativeEvent.clientY - eventElementY,
+        left: e.nativeEvent.clientX - eventElementX,
+        opacity: 0.75,
         display: 'block',
         pointerEvents: 'none'
       });
@@ -39,7 +46,7 @@ function EditableTable() {
 
 
   function updateRows(e) {
-    const b = parseInt(e.nativeEvent.target.id.replace('row',''));
+    const b = parseInt(e.nativeEvent.target.id);
     const isDown = a < b;
 
     let start = a;
@@ -63,21 +70,35 @@ function EditableTable() {
   function mousePressed(e) {
     setMouseDown(true);
     const value = e.nativeEvent.target.innerHTML;
+    setBgColor(e.nativeEvent.target.style.backgroundColor);
+
     let itemOnHold = document.querySelector(".item-on-hold");
     itemOnHold.innerHTML = value;
-    setA(parseInt(e.nativeEvent.target.id.replace("row","")));
+    setA(parseInt(e.nativeEvent.target.id));
+    setOffsetX(e.nativeEvent.offsetX);
+    setOffsetY(e.nativeEvent.offsetY);
+    setItemOnHoldStyle({
+      position: 'absolute',
+      'background-color': bgColor,
+      top: e.nativeEvent.clientY - eventElementY,
+      left: e.nativeEvent.clientX - eventElementX,
+      opacity: 0.75,
+      display: 'block',
+      pointerEvents: 'none'
+    });
   }
 
-  function mouseUp() {
+  function mouseUp(e) {
     setMouseDown(false);
     setItemOnHoldStyle({
       position: 'absolute',
-      background: 'yellow',
-      opacity: 0.3,
+      "background-color": bgColor,
+      opacity: 0.75,
       display: 'none',
       pointerEvents: 'none'
     });
-    console.log("Mouse up");
+    //console.log("Mouse up");
+    updateRows(e);
   }
 
   return (
@@ -88,8 +109,9 @@ function EditableTable() {
             onMouseDown={mousePressed} 
             onMouseUp={mouseUp}
             onMouseMove={mouseMoving}
-            key={i}
-            id={`row${i}`}
+            //key={i}
+            //id={`row${i}`}
+            id={i}
             style={getStyle(i)}>{item}</div>; 
         })
       }
