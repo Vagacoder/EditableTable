da27f2b36133fb9d025f251f6883307ea59a84c7
Lester Dela Cruz <bboyjacks@yahoo.com>
2019-07-22 16:19:43 -0700

    Working Draggable row

--- src/App.js
+++ src/App.js
@@ -1,128 +1,105 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-
-function EditableTable() {
-
-  let cpListItems = ["row 1", "row 2", "row 3", "row 4", "row 5"];
-
-  const [listItems, setListItems] = useState(cpListItems);
-  let colors = ['red', 'green', 'blue', 'orange', 'lightblue'];
-  const [a, setA] = useState(0);
-  const [mouseDown, setMouseDown] = useState(false);
-  const [itemOnHoldStyle, setItemOnHoldStyle] = useState({
-    position: 'absolute',
-    "background-color": bgColor,
-    opacity: 0.75,
-    display: 'none',
-    pointerEvents: 'none'
-  });
-
-  const [bgColor, setBgColor] = useState("yellow");
-  const [eventElementX, setOffsetX] = useState(0);
-  const [eventElementY, setOffsetY] = useState(0);
-
-  function getStyle(i) {
-    return {
-      background: colors[i]
-    };
-  }
-
-  function mouseMoving(e) {
-    if (mouseDown) {
-      setItemOnHoldStyle({
-        position: 'absolute',
-        "background-color": bgColor,
-        //top: e.nativeEvent.clientY - e.nativeEvent.offsetY,
-        //left: e.nativeEvent.clientX - e.nativeEvent.offsetX,
-        top: e.nativeEvent.clientY - eventElementY,
-        left: e.nativeEvent.clientX - eventElementX,
-        opacity: 0.75,
-        display: 'block',
-        pointerEvents: 'none'
-      });
-      updateRows(e);
-    }
-  }
+import './RectHover.css';
 
 
-  function updateRows(e) {
-    const b = parseInt(e.nativeEvent.target.id);
-    const isDown = a < b;
-
-    let start = a;
-    let end = b;
-
-    let temp = cpListItems[start];
-    if (isDown) {
-      for (let i = start; i < end; i++) {
-        cpListItems[i] = cpListItems[i + 1];
-      }
-    } else {
-      for (let i = start; i > end; i--) {
-        cpListItems[i] = cpListItems[i - 1]
-      }
-    }
-
-    cpListItems[end] = temp;
-    setListItems(cpListItems);
-  }
-
-  function mousePressed(e) {
-    setMouseDown(true);
-    const value = e.nativeEvent.target.innerHTML;
-    setBgColor(e.nativeEvent.target.style.backgroundColor);
-
-    let itemOnHold = document.querySelector(".item-on-hold");
-    itemOnHold.innerHTML = value;
-    setA(parseInt(e.nativeEvent.target.id));
-    setOffsetX(e.nativeEvent.offsetX);
-    setOffsetY(e.nativeEvent.offsetY);
-    setItemOnHoldStyle({
-      position: 'absolute',
-      'background-color': bgColor,
-      top: e.nativeEvent.clientY - eventElementY,
-      left: e.nativeEvent.clientX - eventElementX,
-      opacity: 0.75,
-      display: 'block',
-      pointerEvents: 'none'
-    });
-  }
-
-  function mouseUp(e) {
-    setMouseDown(false);
-    setItemOnHoldStyle({
-      position: 'absolute',
-      "background-color": bgColor,
-      opacity: 0.75,
-      display: 'none',
-      pointerEvents: 'none'
-    });
-    //console.log("Mouse up");
-    updateRows(e);
-  }
+function DraggableRow(props) {
+  const r = Math.floor(Math.random() * 255) + 1;
+  const g = Math.floor(Math.random() * 255) + 1;
+  const b = Math.floor(Math.random() * 255) + 1;
+  const color = `rgb(${r}, ${g}, ${b})`;
 
   return (
-    <div className="table">
-      {
-        listItems.map((item, i) => { 
-          return <div 
-            onMouseDown={mousePressed} 
-            onMouseUp={mouseUp}
-            onMouseMove={mouseMoving}
-            //key={i}
-            //id={`row${i}`}
-            id={i}
-            style={getStyle(i)}>{item}</div>; 
-        })
-      }
-      <div className="item-on-hold" style={itemOnHoldStyle}></div>
-    </div>
+    <div 
+      draggable="true" 
+      id={props.index} 
+      onDragOver={props.handleDragOver} 
+      onDragStart={props.handleDragStart}
+      className={props.className} 
+      style={{background: color}}></div>
   );
 }
 
 function App() {
-  return ( 
-    <EditableTable></EditableTable>
+  const numRows = 6;
+  const [startIndex, setStartIndex] = useState();
+  const [endIndex, setEndIndex] = useState();
+  let currentRowOrder = [];
+  for (let i = 0; i < numRows; i++) {
+    currentRowOrder.push(i);
+  }
+
+  const handleDragStart = (e) => {
+    const index = parseInt(e.nativeEvent.target.id);
+    setStartIndex(index);
+  }
+
+  const handleDragOver = (e) => {
+    let elem = e.nativeEvent.target;
+    const index = parseInt(elem.id);
+    setEndIndex(index);
+  }
+
+  useEffect(() => {
+    if (!startIndex && !endIndex)
+      return; 
+    const start = startIndex;
+    const end = endIndex;
+    const temp = currentRowOrder[start];
+    if (start < end) {
+      for (let i = start; i < end; i++) {
+        currentRowOrder[i] = currentRowOrder[i+1];
+      }
+    }
+    else if (start > end) {
+      for (let i = start; i > end; i--) {
+        currentRowOrder[i] = currentRowOrder[i-1];
+      }
+    }
+    currentRowOrder[end] = temp;
+
+    let newRowOrder = [];
+    currentRowOrder.forEach(id => {
+      newRowOrder.push(document.getElementById(`${id}`));
+    })
+
+    let editableTable = document.querySelector(".editable-table");
+    editableTable.innerHTML = "";
+    newRowOrder.forEach(row => {
+      editableTable.appendChild(row);
+    });
+
+    editableTable = document.querySelector(".editable-table");
+    let newRows = editableTable.children;
+    for (let i = 0; i < newRows.length; i++) {
+      newRows[i].setAttribute("id", `${i}`);
+    }
+    currentRowOrder = [0, 1, 2];
+    setStartIndex(endIndex);
+  });
+
+  const [rows, _] = useState(() => {
+    let result = [];
+
+    for (let i = 0; i < numRows; i++) {
+      result.push(
+        <DraggableRow 
+          className={"row blue-background"} 
+          index={i}
+          key={i}
+          handleDragOver={handleDragOver}
+          handleDragStart={handleDragStart}>
+          </DraggableRow>
+      );
+    }
+
+    return result;
+  })
+
+  return (
+    <div  className="editable-table">
+      {rows}
+    </div>
   );
 }
 
