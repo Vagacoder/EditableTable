1350f56b6613e9f3d1e6642409dbbe4d075f48c0
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro.local>
2019-07-24 23:53:50 -0700

    Fail again

--- src/App.js
+++ src/App.js
@@ -1,128 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 
-function EditableTable() {
+import EditableTable from './EditableTable';
 
-  let cpListItems = ["row 1", "row 2", "row 3", "row 4", "row 5"];
-
-  const [listItems, setListItems] = useState(cpListItems);
-  let colors = ['red', 'green', 'blue', 'orange', 'lightblue'];
-  const [a, setA] = useState(0);
-  const [mouseDown, setMouseDown] = useState(false);
-  const [itemOnHoldStyle, setItemOnHoldStyle] = useState({
-    position: 'absolute',
-    "background-color": bgColor,
-    opacity: 0.75,
-    display: 'none',
-    pointerEvents: 'none'
-  });
-
-  const [bgColor, setBgColor] = useState("yellow");
-  const [eventElementX, setOffsetX] = useState(0);
-  const [eventElementY, setOffsetY] = useState(0);
-
-  function getStyle(i) {
-    return {
-      background: colors[i]
-    };
-  }
-
-  function mouseMoving(e) {
-    if (mouseDown) {
-      setItemOnHoldStyle({
-        position: 'absolute',
-        "background-color": bgColor,
-        //top: e.nativeEvent.clientY - e.nativeEvent.offsetY,
-        //left: e.nativeEvent.clientX - e.nativeEvent.offsetX,
-        top: e.nativeEvent.clientY - eventElementY,
-        left: e.nativeEvent.clientX - eventElementX,
-        opacity: 0.75,
-        display: 'block',
-        pointerEvents: 'none'
-      });
-      updateRows(e);
-    }
-  }
-
-
-  function updateRows(e) {
-    const b = parseInt(e.nativeEvent.target.id);
-    const isDown = a < b;
-
-    let start = a;
-    let end = b;
-
-    let temp = cpListItems[start];
-    if (isDown) {
-      for (let i = start; i < end; i++) {
-        cpListItems[i] = cpListItems[i + 1];
-      }
-    } else {
-      for (let i = start; i > end; i--) {
-        cpListItems[i] = cpListItems[i - 1]
-      }
-    }
-
-    cpListItems[end] = temp;
-    setListItems(cpListItems);
-  }
-
-  function mousePressed(e) {
-    setMouseDown(true);
-    const value = e.nativeEvent.target.innerHTML;
-    setBgColor(e.nativeEvent.target.style.backgroundColor);
-
-    let itemOnHold = document.querySelector(".item-on-hold");
-    itemOnHold.innerHTML = value;
-    setA(parseInt(e.nativeEvent.target.id));
-    setOffsetX(e.nativeEvent.offsetX);
-    setOffsetY(e.nativeEvent.offsetY);
-    setItemOnHoldStyle({
-      position: 'absolute',
-      'background-color': bgColor,
-      top: e.nativeEvent.clientY - eventElementY,
-      left: e.nativeEvent.clientX - eventElementX,
-      opacity: 0.75,
-      display: 'block',
-      pointerEvents: 'none'
-    });
-  }
-
-  function mouseUp(e) {
-    setMouseDown(false);
-    setItemOnHoldStyle({
-      position: 'absolute',
-      "background-color": bgColor,
-      opacity: 0.75,
-      display: 'none',
-      pointerEvents: 'none'
-    });
-    //console.log("Mouse up");
-    updateRows(e);
-  }
-
-  return (
-    <div className="table">
-      {
-        listItems.map((item, i) => { 
-          return <div 
-            onMouseDown={mousePressed} 
-            onMouseUp={mouseUp}
-            onMouseMove={mouseMoving}
-            //key={i}
-            //id={`row${i}`}
-            id={i}
-            style={getStyle(i)}>{item}</div>; 
-        })
-      }
-      <div className="item-on-hold" style={itemOnHoldStyle}></div>
-    </div>
-  );
-}
+const shownItems = [true, true, true];
+const headers = ["Name", "Date", "Color"]
+const rowsItems= [
+  [<img className="tbl-itm-img" src="https://source.unsplash.com/random/100x100" alt="img"></img>, "Tom", "red"],
+  [<img className="tbl-itm-img" src="https://source.unsplash.com/random/100x100" alt="img"></img>, "Undress", "green"],
+  [<img className="tbl-itm-img" src="https://source.unsplash.com/random/100x100" alt="img"></img>, "Victor", "blue"],
+]
 
 function App() {
-  return ( 
-    <EditableTable></EditableTable>
+  return (
+    <EditableTable 
+      shownItems={shownItems}
+      headers={headers} 
+      rowItems={rowsItems}></EditableTable>
   );
 }
 
---
+++ src/EditableTable.js
@@ -0,0 +1,156 @@
+import React, { useState } from 'react';
+import './EditableTable.css';
+
+
+function Headers(props) {
+	return (
+		<div className="headers">
+		 	{
+				props.headers.map((header, i) => {
+					return <HeaderItem 
+						key={i}
+						header={header} 
+						index={i} 
+						setShownItems={props.setShownItems}
+						shownItems={props.shownItems}
+					></HeaderItem>
+				})
+			}
+		</div>
+	);
+}
+
+function HeaderItem(props) {
+	const style = props.shownItems[props.index] ? {} : {display: 'none'};
+	const handleOnClick = () => {
+		const shownItems = props.shownItems;
+		const index = props.index;
+		shownItems[index] = !shownItems[index];
+		props.setShownItems(shownItems.slice());
+	}
+
+	return (
+		<div onClick={handleOnClick} style={style} className="header-item">{props.header}</div>
+	);
+}
+
+function Table(props) {
+	return (
+		<div className="table">
+			{
+				props.rowOrder.map((i, index) => {
+					return <TableItem 
+						index={i} 
+						key={index}
+
+						prevEndIndex={props.prevEndIndex}
+						setPrevEndIndex={props.setPrevEndIndex}
+
+						rowOrder={props.rowOrder}
+						setRowOrder={props.setRowOrder}
+						startIndex={props.startIndex}
+						endIndex={props.endIndex}
+						setStartIndex={props.setStartIndex}
+						setEndIndex={props.setEndIndex}
+						shownItems={props.shownItems} 
+						rowItem={props.rowItems[i]}></TableItem>
+				})
+			}
+		</div>
+	);
+}
+
+function TableItem(props) {
+	const handleDragStart = (e) => {
+		const i = parseInt(e.nativeEvent.target.id);
+		props.setStartIndex(i);
+		props.setEndIndex(i);
+	}
+
+	const handleDragOver = (e) => {
+		const targetId = parseInt(e.nativeEvent.target.id);
+		if (isNaN(targetId))
+			return;
+
+		props.setEndIndex(targetId);
+		console.log('start index',  props.startIndex);
+		console.log('end index',  props.endIndex);
+
+		if (props.endIndex != null && 
+			props.startIndex != null) {
+			let rowOrder = props.rowOrder;
+			let temp = rowOrder[props.startIndex];
+			if (props.startIndex < props.endIndex) {
+				for (let i = props.startIndex; i < props.endIndex; i++) {
+					rowOrder[i] = rowOrder[i + 1]
+				}
+			} else {
+				for (let i = props.startIndex; i > props.endIndex; i--) {
+					rowOrder[i] = rowOrder[i - 1];
+				}
+			}
+			rowOrder[props.endIndex] = temp;
+			props.setRowOrder(rowOrder.slice());
+			console.log(props.rowOrder);
+		}
+
+	}
+
+	return (
+		<div id={`${props.index}`} 
+			draggable="true" 
+			className="table-item" 
+			onDragStart={handleDragStart} 
+			onDragOver={handleDragOver}>
+			{
+				props.rowItem.map((item, i) => {
+					return <Item key={i} item={item} index={i} shownItems={props.shownItems}></Item>
+				})
+			}
+		</div>
+	);
+}
+
+function Item(props) {
+	const style = props.shownItems[props.index] ? {} : { display: 'none'};
+	return (
+		<div className="item" style={style}>{props.item}</div>
+	);
+}
+
+function EditableTable(props) {
+	const [shownItems, setShownItems] = useState(props.shownItems);
+	const [rowOrder, setRowOrder] = useState(() => {
+		let result = [];
+		for (let i = 0; i < props.rowItems.length; i++) {
+			result.push(i);
+		}
+		return result;
+	});
+
+	const [startIndex, setStartIndex] = useState();
+	const [endIndex, setEndIndex] = useState();
+	const [prevEndIndex, setPrevEndIndex] = useState(endIndex);
+
+	return (
+		<div className="editable-table"> 
+			<Headers headers={props.headers} shownItems={shownItems} setShownItems={setShownItems}></Headers>
+			<Table 
+				endIndex={endIndex}
+				setEndIndex={setEndIndex}
+
+				startIndex={startIndex}
+				setStartIndex={setStartIndex}
+
+				prevEndIndex={prevEndIndex}
+				setPrevEndIndex={setPrevEndIndex}
+				
+				rowOrder={rowOrder} 
+				setRowOrder={setRowOrder} 
+				rowItems={props.rowItems} 
+				shownItems={shownItems}></Table>
+		</div>
+	);
+}
+
+export default EditableTable;
